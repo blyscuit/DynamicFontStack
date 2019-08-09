257d363258d83140975641cfbfc9a604b279742b
Bliss <pisit.w@23perspective.com>
2019-08-09 14:45:58 +0700

    initial

--- DynamicFontStack/ViewController.swift
+++ DynamicFontStack/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //1
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        //2
+        if previousTraitCollection?.preferredContentSizeCategory != traitCollection.preferredContentSizeCategory {
+            let contentSize = traitCollection.preferredContentSizeCategory
+            if contentSize.isAccessibilityCategory {
+                stackView.axis = .vertical
+                stackView.alignment = .leading
+                stackView.spacing = 2
+            } else {
+                stackView.axis = .horizontal
+                stackView.alignment = .center
+                stackView.spacing = 6
+            }
+        }
+    }
 
 }
 
